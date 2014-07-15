70e1ee61131225e4258c250172059321b723b190
James Robertson <james@r0bertson.co.uk>
2014-07-15 21:18:10 +0100

    feature: Markdown # style headings now implemented.

--- lib/polyrex-headings.rb
+++ lib/polyrex-headings.rb
@@ -10,18 +10,42 @@ class PolyrexHeadings
 
   def initialize(raw_s)
 
-    summary, *s = raw_s.gsub(/(-+\n)\n/,'\1').partition(/[^\n]+\n-+/)
-      
-    a = s.join.lines.chunk{|x| x != "\n" }.map do |record, x| 
-      if record then
-        head = x.shift
-        x.shift
-        [head] + x.map{|y| '  ' + y}
-      end
-    end
+    summary, *s = raw_s.split(/(?=(?:#|[^\n]+\n-+))/,2)
 
+    a = if raw_s =~ /----/ then       
+
+      s.join.lines.chunk{|x| x != "\n" }.map do |record, x| 
+
+        if record then
+
+          head = x.shift
+          x.shift
+          [head] + x.map{|y| '  ' + y}
+        end
+      end
+
+    else
+
+      s.join.split(/^\n*#/)[1..-1].map do |x|  
+
+        lines = x.lines
+
+        raw_heading = lines.shift.rstrip
+        raw_indent = raw_heading.slice!(/#*/)
+        n, heading = raw_indent.length, raw_heading.lstrip
+
+        ([indent(n) + heading] + lines.map{|x| indent(n+1) + x}).join
+      end
+
+    end
     summary.sub!(/^(<\?)(ph|polyrex-headings)/,'\1polyrex')
     @to_polyrex = Polyrex.new.parse(summary + a.join)
+  end
+
+  private
+
+  def indent(n)
+    '  ' * (n)
   end
 
 end
--- polyrex-headings.gemspec
+++ polyrex-headings.gemspec
@@ -1,13 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'polyrex-headings'
-  s.version = '0.1.2'
+  s.version = '0.1.3'
   s.summary = 'polyrex-headings'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
-  s.add_dependency('polyrex') 
+  s.add_runtime_dependency('polyrex', '~> 0.9', '>=0.9.5') 
   s.signing_key = '../privatekeys/polyrex-headings.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
   s.email = 'james@r0bertson.co.uk'
   s.homepage = 'https://github.com/jrobertson/polyrex-headings'
+  s.required_ruby_version = '>= 2.1.0'
 end
