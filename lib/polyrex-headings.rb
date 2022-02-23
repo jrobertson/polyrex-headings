7fb9a7097a59fe0529e2a21b2120a7b870633e70
James Robertson <james@r0bertson.co.uk>
2022-02-23 15:17:47 +0000

    improvement: Now using RXFReader as a replacement for RXFHelper.

--- lib/polyrex-headings.rb
+++ lib/polyrex-headings.rb
@@ -3,6 +3,8 @@
 # file: polyrex-headings.rb
 
 require 'polyrex'
+require 'rxfreader'
+
 
 class PolyrexHeadingsException < Exception
 end
@@ -12,28 +14,28 @@ class PolyrexHeadings
   attr_reader :to_polyrex, :to_px, :to_s
 
   def initialize(raw_s, debug: false)
-    
-    @debug = debug
-    buffer, type = RXFHelper.read(raw_s)
-    
-    if type == :unknown and buffer.lines.length <= 1 then
-      raise PolyrexHeadingsException, 'File not found' 
-    end    
 
-    # add an empty space to the blank line where there is no 
+    @debug = debug
+    buffer, type = RXFReader.read(raw_s)
+
+    if type == :unknown and buffer.lines.length <= 1 then
+      raise PolyrexHeadingsException, 'File not found'
+    end
+
+    # add an empty space to the blank line where there is no
     # raw record between headings
-    
+
     buffer.gsub!(/(#+[^\n]+\n+)(?=\n#)/m,'\1 ')
     summary, *s = buffer.split(/(?=(?:^#|[\n]+\n-+))/,2)
     type = nil
 
-    a = if raw_s =~ /----/ then       
-    
-      # this was the original code which used dashes to make an underline 
+    a = if raw_s =~ /----/ then
+
+      # this was the original code which used dashes to make an underline
       # of a heading. Typically the heading is now prefixed with a hash (#)
       # which means this block is unlikely to be executed.
 
-      s.join.lines.chunk{|x| x != "\n" }.map do |record, x| 
+      s.join.lines.chunk{|x| x != "\n" }.map do |record, x|
 
         if record then
 
@@ -44,25 +46,25 @@ class PolyrexHeadings
       end
 
     else
-      
+
       a2 = summary.gsub(/\n{2,}/,"\n").lines
 
       if a2.last =~ /^--+/ then
         a2.pop
         summary = a2.join
-        type = :rowx 
+        type = :rowx
       end
-        
-      s.join.split(/^\n*#/)[1..-1].map do |x|  
-        
+
+      s.join.split(/^\n*#/)[1..-1].map do |x|
+
         lines = x.lines
-        raw_heading = lines.shift.rstrip        
-        
+        raw_heading = lines.shift.rstrip
+
         body = lines.join.strip
 
         if type == :rowx and body.length > 0
 
-          lines2 = ["\n"] + 
+          lines2 = ["\n"] +
               RowX.new(body, level: 0).to_lines(delimiter: ' # ') << "\n"
         else
 
@@ -72,28 +74,28 @@ class PolyrexHeadings
         raw_indent = raw_heading.slice!(/#*/)
         n, heading = raw_indent.length, raw_heading.lstrip
 
-        ([indent(n) + heading] + lines2.map{|x| indent(n+1) + x}).join        
+        ([indent(n) + heading] + lines2.map{|x| indent(n+1) + x}).join
       end
 
     end
 
     summary.sub!(/^(<\?)(ph|polyrex-headings)/,'\1polyrex')
     @to_s = string = summary + a.join
-    
+
     px = Polyrex.new
 
     px.parse(string, delimiter: ' # ')
-    
+
     @to_polyrex = @to_px = @px = px
 
   end
-  
+
   def to_h(symbolise: true)
     build(@px.records, symbolise)
   end
-  
+
   private
-  
+
   def build(records, symbolise)
 
     a = records.map do |record|
@@ -102,7 +104,7 @@ class PolyrexHeadings
         puts 'record.records: ' + record.records.inspect
         puts 'record.x: ' + record.x.inspect
       end
-      
+
       if record.records.any? then
 
         key = symbolise ? record.x.downcase.to_sym : record.x
@@ -124,7 +126,7 @@ class PolyrexHeadings
     puts 'a.first: ' + a.first.inspect if @debug
     return a.inject({}) {|r,x| r.merge!(x)} if a.first.is_a? Hash
 
-    a      
+    a
 
   end
 
--- polyrex-headings.gemspec
+++ polyrex-headings.gemspec
@@ -1,14 +1,15 @@
 Gem::Specification.new do |s|
   s.name = 'polyrex-headings'
-  s.version = '0.2.0'
-  s.summary = 'Parses a raw Polyrex document containing Markdown style headings'
+  s.version = '0.3.0'
+  s.summary = 'Parses a raw Polyrex document containing Markdown ' \
+      + ' style headings'
   s.authors = ['James Robertson']
   s.files = Dir['lib/polyrex-headings.rb']
-  s.add_runtime_dependency('polyrex', '~> 1.3', '>=1.3.0') 
+  s.add_runtime_dependency('polyrex', '~> 1.4', '>=1.4.0')
   s.signing_key = '../privatekeys/polyrex-headings.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/polyrex-headings'
   s.required_ruby_version = '>= 2.3.0'
 end
